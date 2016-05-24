d3745f525bbb393bd0b8c313bf84a21e2d17f84b
mathewgabsam <abraham_carandang@dlsu.edu.ph>
2016-05-24 16:36:29 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	printf("\n         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("\n               Abraham Matthew S. Carandang\n");
+	printf("\n                         BS CIV\n");
+	printf("\n                        11412674\n");
+	printf("\n                      May 24, 2016\n");
+	printf("\n             matthew_carandang@yahoo.com\n");
+	printf("\n                      09167364610\n");
+	printf("\n         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	
+	system("PAUSE");
+	return 0;
+}
